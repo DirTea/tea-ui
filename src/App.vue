c42853c986408644d30564e915432ca4c8d0ba94
DIRTEA-小新16PR\DirTea <571918762@qq.com>
2024-09-27 10:31:41 +0800

    tea-roller：写不好，删除 tea-e-sign：新增电子签名组件

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,93 @@
 <template>
-  <tea-dialog v-model="dialogVisible" background="/src/assets/vue.svg">
-    <div style="width: 500px;height: 500px"> 这是弹出框，请自定义</div>
-  </tea-dialog>
-  <tea-button @click="dialogVisible = true">弹出框</tea-button>
+  <div id="app" class="esign">
+    <div class="esign-box">
+      <tea-e-sign
+        class="esignature"
+        ref="esign"
+        :width="800"
+        :height="300"
+        :isCrop="isCrop"
+        :lineWidth="lineWidth"
+        :lineColor="lineColor"
+        :bgColor.sync="bgColor"
+      />
+    </div>
+    <div class="esigh-btns">
+      <button @click="handleReset">清空画板</button>
+      <button @click="handleGenerate">生成图片</button>
+    </div>
+    <div class="esigh-result">
+      <img v-if="resultImg" :src="resultImg" alt="" />
+    </div>
+  </div>
 </template>
 
-<script setup>
-import {ref} from 'vue';
-import TeaDialog from "./components/dialog/TeaDialog.vue";
-import TeaButton from "./components/button/vue2/TeaButton.vue";
-let dialogVisible = ref(false);
+<script>
+import TeaESign from "./components/e-sign/vue3/TeaESign.vue";
+
+export default {
+  name: "App",
+  components: { TeaESign },
+  data() {
+    return {
+      lineWidth: 6,
+      lineColor: "#000000",
+      bgColor: "",
+      resultImg: "",
+      isCrop: false,
+    };
+  },
+
+  methods: {
+    handleReset() {
+      this.$refs.esign.reset();
+    },
+    handleGenerate() {
+      this.$refs.esign
+        .generate()
+        .then((res) => {
+          this.resultImg = res;
+        })
+        .catch((err) => {
+          alert(err);
+        });
+    },
+  },
+};
 </script>
 
 <style scoped>
-
+.esign {
+  max-width: 1000px;
+  margin: auto;
+  padding: 10px;
+}
+.esigh-btns button {
+  color: #fff;
+  height: 40px;
+  width: 100px;
+  font-size: 16px;
+  margin-right: 10px;
+  outline: none;
+  border-radius: 4px;
+  background: #f60;
+  border: 1px solid transparent;
+}
+.esigh-btns button:active {
+  color: #fff;
+  box-shadow: 0px 0px 50px orangered inset;
+}
+.esigh-result {
+  margin-top: 10px;
+}
+.esigh-result img {
+  display: block;
+  max-width: 100%;
+  height: auto;
+  border: 1px solid #ececee;
+}
+.esignature {
+  margin: 10px 0;
+  border: 2px solid #ccc;
+}
 </style>
---
+++ src/components/e-sign/vue3/TeaESign.vue
@@ -0,0 +1,342 @@
+<template>
+  <canvas
+    ref="canvasRef"
+    @mousedown="mouseDown"
+    @mousemove="mouseMove"
+    @mouseup="mouseUp"
+    @touchstart="touchStart"
+    @touchmove="touchMove"
+    @touchend="touchEnd"
+  ></canvas>
+</template>
+
+<script setup>
+import {
+  computed,
+  onBeforeMount,
+  onBeforeUnmount,
+  onMounted,
+  ref,
+  watch,
+} from "vue";
+
+const props = defineProps({
+  width: {
+    type: Number,
+    default: 800,
+  },
+  height: {
+    type: Number,
+    default: 300,
+  },
+  lineWidth: {
+    type: Number,
+    default: 4,
+  },
+  lineColor: {
+    type: String,
+    default: "#000000",
+  },
+  bgColor: {
+    type: String,
+    default: "",
+  },
+  isCrop: {
+    type: Boolean,
+    default: false,
+  },
+  isClearBgColor: {
+    type: Boolean,
+    default: true,
+  },
+  format: {
+    type: String,
+    default: "image/png",
+  },
+  quality: {
+    type: Number,
+    default: 1,
+  },
+});
+
+const hasDrew = ref(false);
+const resultImg = ref("");
+const points = ref([]);
+const canvasTxt = ref(null);
+const startX = ref(0);
+const startY = ref(0);
+const isDrawing = ref(false);
+const sratio = ref(1);
+
+const ratio = computed(() => {
+  return props.height / props.width;
+});
+const canvasRef = ref();
+const stageInfo = computed(() => {
+  return canvasRef.value.getBoundingClientRect();
+});
+const myBg = computed(() => {
+  return props.bgColor ? props.bgColor : "rgba(255, 255, 255, 0)";
+});
+
+watch(
+  () => myBg.value,
+  (newVal) => {
+    canvasRef.value.style.background = newVal;
+  },
+);
+
+onBeforeMount(() => {
+  window.addEventListener("resize", $_resizeHandler);
+});
+onBeforeUnmount(() => {
+  window.removeEventListener("resize", $_resizeHandler);
+});
+onMounted(() => {
+  const canvas = canvasRef.value;
+  canvas.height = props.height;
+  canvas.width = props.width;
+  canvas.style.background = myBg.value;
+  $_resizeHandler();
+  // 在画板以外松开鼠标后冻结画笔
+  document.onmouseup = () => {
+    isDrawing.value = false;
+  };
+});
+
+const $_resizeHandler = () => {
+  const canvas = canvasRef.value;
+  canvas.style.width = props.width + "px";
+  const realw = parseFloat(window.getComputedStyle(canvas).width);
+  canvas.style.height = ratio.value * realw + "px";
+  canvasTxt.value = canvas.getContext("2d");
+  canvasTxt.value.scale(1 * sratio.value, 1 * sratio.value);
+  sratio.value = realw / props.width;
+  canvasTxt.value.scale(1 / sratio.value, 1 / sratio.value);
+};
+// pc
+const mouseDown = (e) => {
+  e = e || event;
+  e.preventDefault();
+  isDrawing.value = true;
+  hasDrew.value = true;
+  let obj = {
+    x: e.offsetX,
+    y: e.offsetY,
+  };
+  drawStart(obj);
+};
+const mouseMove = (e) => {
+  e = e || event;
+  e.preventDefault();
+  if (isDrawing.value) {
+    let obj = {
+      x: e.offsetX,
+      y: e.offsetY,
+    };
+    drawMove(obj);
+  }
+};
+const mouseUp = (e) => {
+  e = e || event;
+  e.preventDefault();
+  let obj = {
+    x: e.offsetX,
+    y: e.offsetY,
+  };
+  drawEnd(obj);
+  isDrawing.value = false;
+};
+// mobile
+const touchStart = (e) => {
+  e = e || event;
+  e.preventDefault();
+  hasDrew.value = true;
+  if (e.touches.length === 1) {
+    let obj = {
+      x:
+        e.targetTouches[0].clientX -
+        canvasRef.value.getBoundingClientRect().left,
+      y:
+        e.targetTouches[0].clientY -
+        canvasRef.value.getBoundingClientRect().top,
+    };
+    drawStart(obj);
+  }
+};
+const touchMove = (e) => {
+  e = e || event;
+  e.preventDefault();
+  if (e.touches.length === 1) {
+    let obj = {
+      x:
+        e.targetTouches[0].clientX -
+        canvasRef.value.getBoundingClientRect().left,
+      y:
+        e.targetTouches[0].clientY -
+        canvasRef.value.getBoundingClientRect().top,
+    };
+    drawMove(obj);
+  }
+};
+const touchEnd = (e) => {
+  e = e || event;
+  e.preventDefault();
+  if (e.touches.length === 1) {
+    let obj = {
+      x:
+        e.targetTouches[0].clientX -
+        canvasRef.value.getBoundingClientRect().left,
+      y:
+        e.targetTouches[0].clientY -
+        canvasRef.value.getBoundingClientRect().top,
+    };
+    drawEnd(obj);
+  }
+};
+// 绘制
+const drawStart = (obj) => {
+  startX.value = obj.x;
+  startY.value = obj.y;
+  canvasTxt.value.beginPath();
+  canvasTxt.value.moveTo(startX.value, startY.value);
+  canvasTxt.value.lineTo(obj.x, obj.y);
+  canvasTxt.value.lineCap = "round";
+  canvasTxt.value.lineJoin = "round";
+  canvasTxt.value.lineWidth = props.lineWidth * sratio.value;
+  canvasTxt.value.stroke();
+  canvasTxt.value.closePath();
+  points.value.push(obj);
+};
+const drawMove = (obj) => {
+  canvasTxt.value.beginPath();
+  canvasTxt.value.moveTo(startX.value, startY.value);
+  canvasTxt.value.lineTo(obj.x, obj.y);
+  canvasTxt.value.strokeStyle = props.lineColor;
+  canvasTxt.value.lineWidth = props.lineWidth * sratio.value;
+  canvasTxt.value.lineCap = "round";
+  canvasTxt.value.lineJoin = "round";
+  canvasTxt.value.stroke();
+  canvasTxt.value.closePath();
+  startY.value = obj.y;
+  startX.value = obj.x;
+  points.value.push(obj);
+};
+const drawEnd = (obj) => {
+  canvasTxt.value.beginPath();
+  canvasTxt.value.moveTo(startX.value, startY.value);
+  canvasTxt.value.lineTo(obj.x, obj.y);
+  canvasTxt.value.lineCap = "round";
+  canvasTxt.value.lineJoin = "round";
+  canvasTxt.value.stroke();
+  canvasTxt.value.closePath();
+  points.value.push(obj);
+  points.value.push({ x: -1, y: -1 });
+};
+// 操作
+const generate = (options) => {
+  let imgFormat = options && options.format ? options.format : props.format;
+  let imgQuality = options && options.quality ? options.quality : props.quality;
+  const pm = new Promise((resolve, reject) => {
+    if (!hasDrew.value) {
+      reject(`Warning: Not Signned!`);
+      return;
+    }
+    var resImgData = canvasTxt.value.getImageData(
+      0,
+      0,
+      canvasRef.value.width,
+      canvasRef.value.height,
+    );
+    canvasTxt.value.globalCompositeOperation = "destination-over";
+    canvasTxt.value.fillStyle = myBg.value;
+    canvasTxt.value.fillRect(
+      0,
+      0,
+      canvasRef.value.width,
+      canvasRef.value.height,
+    );
+    resultImg.value = canvasRef.value.toDataURL(imgFormat, imgQuality);
+    canvasTxt.value.clearRect(
+      0,
+      0,
+      canvasRef.value.width,
+      canvasRef.value.height,
+    );
+    canvasTxt.value.putImageData(resImgData, 0, 0);
+    canvasTxt.value.globalCompositeOperation = "source-over";
+    if (props.isCrop) {
+      const crop_area = getCropArea(resImgData.data);
+      var crop_canvas = document.createElement("canvas");
+      const crop_ctx = crop_canvas.getContext("2d");
+      crop_canvas.width = crop_area[2] - crop_area[0];
+      crop_canvas.height = crop_area[3] - crop_area[1];
+      const crop_imgData = canvasTxt.value.getImageData(...crop_area);
+      crop_ctx.globalCompositeOperation = "destination-over";
+      crop_ctx.putImageData(crop_imgData, 0, 0);
+      crop_ctx.fillStyle = myBg.value;
+      crop_ctx.fillRect(0, 0, crop_canvas.width, crop_canvas.height);
+      resultImg.value = crop_canvas.toDataURL(imgFormat, imgQuality);
+      crop_canvas = null;
+    }
+    resolve(resultImg.value);
+  });
+  return pm;
+};
+const emit = defineEmits(["update:bgColor"]);
+const reset = () => {
+  canvasTxt.value.clearRect(
+    0,
+    0,
+    canvasRef.value.width,
+    canvasRef.value.height,
+  );
+  if (props.isClearBgColor) {
+    emit("update:bgColor", "");
+    canvasRef.value.style.background = "rgba(255, 255, 255, 0)";
+  }
+  points.value = [];
+  hasDrew.value = false;
+  resultImg.value = "";
+};
+const getCropArea = (imgData) => {
+  var topX = canvasRef.value.width;
+  var btmX = 0;
+  var topY = canvasRef.value.height;
+  var btnY = 0;
+  for (var i = 0; i < canvasRef.value.width; i++) {
+    for (var j = 0; j < canvasRef.value.height; j++) {
+      var pos = (i + canvasRef.value.width * j) * 4;
+      if (
+        imgData[pos] > 0 ||
+        imgData[pos + 1] > 0 ||
+        imgData[pos + 2] ||
+        imgData[pos + 3] > 0
+      ) {
+        btnY = Math.max(j, btnY);
+        btmX = Math.max(i, btmX);
+        topY = Math.min(j, topY);
+        topX = Math.min(i, topX);
+      }
+    }
+  }
+  topX++;
+  btmX++;
+  topY++;
+  btnY++;
+  const data = [topX, topY, btmX, btnY];
+  return data;
+};
+
+defineExpose({
+  reset,
+  generate,
+});
+</script>
+
+<style scoped>
+canvas {
+  max-width: 100%;
+  display: block;
+}
+</style>
